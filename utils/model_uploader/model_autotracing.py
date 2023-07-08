77f7a97652273eec575362ce174e6c2adc199eda
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-07-08 06:30:24 +0000

    Set up cluster for auto-tracing

---
+++ utils/model_uploader/model_autotracing.py
@@ -0,0 +1,81 @@
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Any modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+
+import argparse
+from opensearchpy import OpenSearch
+from opensearch_py_ml.ml_commons import MLCommonClient
+from opensearch_py_ml.ml_commons.model_uploader import ModelUploader
+from opensearch_py_ml.ml_models.sentencetransformermodel import SentenceTransformerModel
+from tests import OPENSEARCH_TEST_CLIENT
+import warnings
+
+TORCH_SCRIPT_FORMAT = 'TORCH_SCRIPT'
+ONNX_FORMAT = 'ONNX'
+TORCHSCRIPT_FOLDER_PATH = "sentence-transformers-torchscript/"
+ONXX_FOLDER_PATH = "sentence-transformers-onxx/"
+MODEL_CONFIG_FILE_NAME = "ml-commons_model_config.json"
+TEST_SENTENCES = ["First test sentence", "Second test sentence"]
+RTOL_TEST = 1e-03
+ATOL_TEST = 1e-05
+ML_BASE_URI = "/_plugins/_ml"
+
+def main(args):
+    print("ARGUMENTS")
+    print(
+        args.model_id,
+        args.model_version,
+        args.tracing_format,
+        args.embedding_dimension,
+        args.pooling_mode,
+    )
+    ml_client = MLCommonClient(OPENSEARCH_TEST_CLIENT)
+    
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings('ignore', category=DeprecationWarning)
+    warnings.filterwarnings('ignore', category=FutureWarning)
+    warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+    warnings.filterwarnings("ignore", message="TracerWarning: torch.tensor")
+    warnings.filterwarnings("ignore", message="using SSL with verify_certs=False is insecure.")
+    
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "model_id",
+        type=str,
+        help="Model ID for auto-tracing and uploading (e.g. sentence-transformers/msmarco-distilbert-base-tas-b)",
+    )
+    parser.add_argument(
+        "model_version", type=str, help="Model version number (e.g. 1.0.1)"
+    )
+    parser.add_argument(
+        "tracing_format",
+        choices=["BOTH", "TORCH_SCRIPT", "ONNX"],
+        help="Model format for auto-tracing",
+    )
+    parser.add_argument(
+        "-ed",
+        "--embedding_dimension",
+        type=int,
+        nargs="?",
+        default=None,
+        const=None,
+        help="Embedding dimension of the model to use if it does not exist in original config.json",
+    )
+    parser.add_argument(
+        "-pm",
+        "--pooling_mode",
+        type=str,
+        nargs="?",
+        default=None,
+        const=None,
+        choices=["CLS", "MEAN", "MAX", "MEAN_SQRT_LEN"],
+        help="Pooling mode if it does not exist in original config.json",
+    )
+    args = parser.parse_args()
+
+    main(args)
