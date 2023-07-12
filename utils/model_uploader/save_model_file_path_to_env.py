e27116453df6fac2e21809931917c09a2464de0d
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-07-12 13:06:04 -0700

    Edit

--- utils/model_uploader/save_model_file_path_to_env.py
+++ utils/model_uploader/save_model_file_path_to_env.py
@@ -1,15 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Any modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+
 import argparse
 import os
+
 
 def get_model_file_path(model_id, model_version, model_format):
     model_name = str(model_id.split("/")[-1])
     model_format = model_format.lower()
     model_dirname = f"{model_name}/{model_version}/{model_format}"
     model_filename = (
-            f"sentence-transformers_{model_name}-{model_version}-{model_format}.zip"
-        )
+        f"sentence-transformers_{model_name}-{model_version}-{model_format}.zip"
+    )
     model_file_path = model_dirname + "/" + model_filename
     return model_file_path
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -29,10 +38,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    model_file_path = get_model_file_path(args.model_id, args.model_version, args.model_format)
-    print(model_file_path)
-    #env_file = os.getenv('GITHUB_ENV')
-    #var_name = "TORCH_FILE_PATH" if model_format == "TORCH_SCRIPT" else "ONNX_FILE_PATH"
+    model_file_path = get_model_file_path(
+        args.model_id, args.model_version, args.model_format
+    )
 
-    #with open(env_file, "a") as f:
-    #    f.write(f"{var_name}={model_file_path}")
+    env_file = os.getenv('GITHUB_ENV')
+    var_name = "TORCH_FILE_PATH" if model_format == "TORCH_SCRIPT" else "ONNX_FILE_PATH"
+    with open(env_file, "a") as f:
+        f.write(f"{var_name}={model_file_path}")
