c1061f33c95f43ddf28403457da61d3ef18fb21c
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-07-21 15:12:31 -0700

    Update update_changelog_md.py
    Signed-off-by: Thanawan Atchariyachanvanit <latchari@amazon.com>

--- utils/model_uploader/update_changelog_md.py
+++ utils/model_uploader/update_changelog_md.py
@@ -6,8 +6,8 @@
 # GitHub history for details.
 
 # This program is run by "Model Auto-tracing & Uploading" workflow
-# (See model_uploader.yml) to update MODEL_UPLOAD_HISTORY.md & supported_models.json
-# after uploading the model to our model hub.
+# (See model_uploader.yml) to update CHANGELOG.md after uploading the model 
+# to our model hub.
 
 import argparse
 from mdutils.fileutils import MarkDownFile
@@ -17,28 +17,13 @@ CHANGELOG_FILENAME = "CHANGELOG.md"
 SECTION_NAME = "Added"
 
 def update_changelog_file(
-    model_id: str,
-    model_version: str,
-    tracing_format: str,
-    workflow_initiator: str,
-    pr_number: int,
-    pr_url: str,
+    changelog_line: str,
 ) -> None:
     """
     Update supported_models.json
 
-    :param model_id: Model ID of the pretrained model
-    :type model_id: string
-    :param model_version: Version of the pretrained model for registration
-    :type model_version: string
-    :param tracing_format: Tracing format ("TORCH_SCRIPT", "ONNX", or "BOTH")
-    :type tracing_format: string
-    :param workflow_initiator: Workflow initiator
-    :type workflow_initiator: string
-    :param pr_number: Pull request number
-    :type pr_number: int
-    :param pr_url: Pull request URL
-    :type pr_url: string
+    :param changelog_line: Line to be added to CHANGELOG.md
+    :type changelog_line: string
     """
     changelog_data = MarkDownFile.read_file(f"{CHANGELOG_DIRNAME}/{CHANGELOG_FILENAME}")
     
@@ -48,19 +33,15 @@ def update_changelog_file(
     this_version_section = changelog_data[this_version_ptr:next_version_ptr]
     
     this_subsection_ptr = newest_version.find(f'### {SECTION_NAME}')
-    
-    new_line = f"Update model upload history - {model_id} (v.{model_version})({tracing_format}) by @{workflow_initiator} ([#{pr_number}]({pr_url}))"
-    
     if this_subsection_ptr != -1:
         next_subsection_ptr = newest_version.find('### ', this_subsection_ptr + 1)
         this_subsection = this_version_section[this_subsection_ptr:next_subsection_ptr].strip()
-        this_subsection += '\n- ' + new_line + '\n\n'
+        this_subsection += '\n- ' + changelog_line + '\n\n'
         new_version_section = this_version_section[:this_subsection_ptr] + this_subsection + this_version_section[next_subsection_ptr:]
     else:
         this_subsection = this_version_section.strip()
-        this_subsection += '\n\n' + f'### {SECTION_NAME}\n- ' + new_line + '\n\n'
+        this_subsection += '\n\n' + f'### {SECTION_NAME}\n- ' + changelog_line + '\n\n'
         new_version_section = this_subsection
-        
    
     new_changelog_data = file_data[:this_version_ptr] + new_version_section + file_data[next_version_ptr:]
     
@@ -71,42 +52,10 @@ def update_changelog_file(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "model_id",
+        "changelog_line",
         type=str,
-        help="Model ID for auto-tracing and uploading (e.g. sentence-transformers/msmarco-distilbert-base-tas-b)",
+        help="Line to be added to CHANGELOG.md",
     )
     parser.add_argument(
-        "model_version", type=str, help="Model version number (e.g. 1.0.1)"
-    )
-    parser.add_argument(
-        "tracing_format",
-        choices=["BOTH", "TORCH_SCRIPT", "ONNX"],
-        help="Model format for auto-tracing",
-    )
-    parser.add_argument(
-        "workflow_initiator",
-        type=str,
-        help="Workflow Initiator",
-    )
-    parser.add_argument(
-        "pr_number",
-        type=int,
-        help="Pull request number",
-    )
-
-    parser.add_argument(
-        "pr_url",
-        type=str,
-        help="Pull request URL",
-    )
-
     args = parser.parse_args()
-
-    update_changelog_file(
-        args.model_id,
-        args.model_version,
-        args.tracing_format,
-        arg.workflow_initiator,
-        args.pr_number,
-        args.pr_url,
-    )
+    update_changelog_file(args.changelog_line)
