3d1c5877e696a11ca947900c2a31057601c868f5
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-07-21 14:52:47 -0700

    Implement update_changelog_md.py
    Signed-off-by: Thanawan Atchariyachanvanit <latchari@amazon.com>

--- utils/model_uploader/update_changelog_md.py
+++ utils/model_uploader/update_changelog_md.py
@@ -10,17 +10,17 @@
 # after uploading the model to our model hub.
 
 import argparse
-
 from mdutils.fileutils import MarkDownFile
-from mdutils.tools.Table import Table
 
-CHANGELOG_FILENAME = "CHANGELOG.MD"
-
+CHANGELOG_DIRNAME = "."
+CHANGELOG_FILENAME = "CHANGELOG.md"
+SECTION_NAME = "Added"
 
 def update_changelog_file(
     model_id: str,
     model_version: str,
     tracing_format: str,
+    workflow_initiator: str,
     pr_number: int,
     pr_url: str,
 ) -> None:
@@ -33,14 +33,39 @@ def update_changelog_file(
     :type model_version: string
     :param tracing_format: Tracing format ("TORCH_SCRIPT", "ONNX", or "BOTH")
     :type tracing_format: string
+    :param workflow_initiator: Workflow initiator
+    :type workflow_initiator: string
     :param pr_number: Pull request number
     :type pr_number: int
     :param pr_url: Pull request URL
     :type pr_url: string
     """
-    mdFile = MarkDownFile(CHANGELOG_FILENAME)
-    mdFile_str = mdFile.read_file()
-    mdFile.rewrite_all_file(data=mdFile_str)
+    changelog_data = MarkDownFile.read_file(f"{CHANGELOG_DIRNAME}/{CHANGELOG_FILENAME}")
+    
+    this_version_ptr = changelog_data.find('## [')
+    assert this_version_ptr != -1, "Cannot find a version section in the CHANGELOG.md"
+    next_version_ptr = changelog_data.find('## [', this_version_ptr + 1)
+    this_version_section = changelog_data[this_version_ptr:next_version_ptr]
+    
+    this_subsection_ptr = newest_version.find(f'### {SECTION_NAME}')
+    
+    new_line = f"Update model upload history - {model_id} (v.{model_version})({tracing_format}) by @{workflow_initiator} ([#{pr_number}]({pr_url}))"
+    
+    if this_subsection_ptr != -1:
+        next_subsection_ptr = newest_version.find('### ', this_subsection_ptr + 1)
+        this_subsection = this_version_section[this_subsection_ptr:next_subsection_ptr].strip()
+        this_subsection += '\n- ' + new_line + '\n\n'
+        new_version_section = this_version_section[:this_subsection_ptr] + this_subsection + this_version_section[next_subsection_ptr:]
+    else:
+        this_subsection = this_version_section.strip()
+        this_subsection += '\n\n' + f'### {SECTION_NAME}\n- ' + new_line + '\n\n'
+        new_version_section = this_subsection
+        
+   
+    new_changelog_data = file_data[:this_version_ptr] + new_version_section + file_data[next_version_ptr:]
+    
+    mdFile = MarkDownFile(CHANGELOG_FILENAME, dirname=CHANGELOG_DIRNAME)
+    mdFile.rewrite_all_file(data=new_changelog_data)
 
 
 if __name__ == "__main__":
@@ -58,7 +83,11 @@ if __name__ == "__main__":
         choices=["BOTH", "TORCH_SCRIPT", "ONNX"],
         help="Model format for auto-tracing",
     )
-
+    parser.add_argument(
+        "workflow_initiator",
+        type=str,
+        help="Workflow Initiator",
+    )
     parser.add_argument(
         "pr_number",
         type=int,
@@ -77,6 +106,7 @@ if __name__ == "__main__":
         args.model_id,
         args.model_version,
         args.tracing_format,
+        arg.workflow_initiator,
         args.pr_number,
         args.pr_url,
     )
