0080b6f241fd475bdc02c0461daeb95d32e335a7
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-08-03 13:28:43 -0700

    Update model_autotracing.py
    Signed-off-by: Thanawan Atchariyachanvanit <latchari@amazon.com>

--- utils/model_uploader/model_autotracing.py
+++ utils/model_uploader/model_autotracing.py
@@ -535,6 +535,9 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    if args.model_description == "":
+        args.model_description = None
+        
     main(
         args.model_id,
         args.model_version,
